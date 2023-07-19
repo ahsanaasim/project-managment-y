b53eb397e14b82929bcaf6983a1058574c869a60
Bhavan Kumar <bhavankumarcse2020@gmail.com>
2023-07-19 20:16:44 +0530

    firestore initialized

--- app/project/1/page.tsx
+++ app/project/1/page.tsx
@@ -3,17 +3,27 @@
 import Wrapper from "@/app/components/Wrapper";
 import { Button, Input, Space, Typography } from "antd";
 import ProjectList from "./ProjectList";
+import { FormEventHandler, useState } from "react";
 
 const page = () => {
+  const [name, setName] = useState("");
+
+  const createProject:FormEventHandler = (e) => {
+    e.preventDefault();
+
+    // save in firestore
+    
+  }
+
   return <Wrapper>
     <Typography.Title>Create a New Project</Typography.Title>
-    <form>
+    <form onSubmit={createProject}>
         <Space direction="vertical" size="large" style={{ display:"flex", width:"50%"}}>
             <Space direction="vertical" style={{display:"flex"}}>
                 <label htmlFor="project-name">Project Name</label>
-                <Input name="project-name" id="project-name" />
+                <Input value={name} onChange={e=>setName(e.target.value)} name="project-name" id="project-name" />
             </Space>
-            <Button type="primary" block>Create Project</Button>
+            <Button htmlType="submit" type="primary" block>Create Project</Button>
         </Space>
     </form>
     <ProjectList/>
