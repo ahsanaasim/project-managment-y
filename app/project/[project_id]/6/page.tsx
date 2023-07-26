590a712d057e162093e2a463344750b3a8daeb49
Bhavan Kumar <bhavankumarcse2020@gmail.com>
2023-07-26 09:50:15 +0530

    step 3 crud bug2 fixed

--- app/project/[project_id]/6/page.tsx
+++ app/project/[project_id]/6/page.tsx
@@ -1,25 +1,29 @@
 "use client";
-import Wrapper from '@/app/components/Wrapper'
-import { UploadOutlined } from '@ant-design/icons'
-import { Button, Typography, Upload } from 'antd'
-import { UploadChangeParam, UploadFile } from 'antd/es/upload';
-import React from 'react'
-import ProjectFlowFooter from '../ProjectFlowFooter';
+import Wrapper from "@/app/components/Wrapper";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Typography, Upload } from "antd";
+import { UploadChangeParam, UploadFile } from "antd/es/upload";
+import React, { FormEventHandler } from "react";
+import ProjectFlowFooter from "../ProjectFlowFooter";
 
 const page = () => {
-    const onFileUpload = (info: UploadChangeParam<UploadFile<any>>) => {
-        console.log(info);
-    }
+  const onFileUpload = (info: UploadChangeParam<UploadFile<any>>) => {
+    console.log(info);
+  };
 
-  return <Wrapper>
-    <Typography.Title>Upload Documents</Typography.Title>
-    <Upload onChange={onFileUpload} multiple>
+  const uploadDocuments: FormEventHandler = async (e) => {};
+
+  return (
+    <Wrapper>
+      <Typography.Title>Upload Documents</Typography.Title>
+      <Upload onChange={onFileUpload} multiple>
         <Button icon={<UploadOutlined />}>Click to Upload</Button>
-    </Upload>
-    <br />
-    <Button type='primary'>Generate Recommendations</Button>
-    <ProjectFlowFooter previous={5} next={7} />
-  </Wrapper>
-}
+      </Upload>
+      <br />
+      <Button type="primary">Generate Recommendations</Button>
+      <ProjectFlowFooter previous={5} submitForm={uploadDocuments} />
+    </Wrapper>
+  );
+};
 
-export default page
+export default page;
